cdad7f5853ea7c75709788a1b29f362d2f71af60
BrilAlex <brilalex.it@yandex.ru>
2021-09-20 00:50:45 +0300

    Updates

---
+++ precache-manifest.36a9614d1ee0eaabc563d52b27f2add7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cbfd5d9f24499a19d7438d987295a3",
+    "url": "/ignat-homeworks-ts-project/index.html"
+  },
+  {
+    "revision": "f7794b7937a5c6c691fb",
+    "url": "/ignat-homeworks-ts-project/static/css/main.a73051da.chunk.css"
+  },
+  {
+    "revision": "a7e068627fabb5288d16",
+    "url": "/ignat-homeworks-ts-project/static/js/2.77847639.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ignat-homeworks-ts-project/static/js/2.77847639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7794b7937a5c6c691fb",
+    "url": "/ignat-homeworks-ts-project/static/js/main.44a30fea.chunk.js"
+  },
+  {
+    "revision": "e0167f4f2bf64f0f8a8c",
+    "url": "/ignat-homeworks-ts-project/static/js/runtime-main.89fc013d.js"
+  }
+]);
