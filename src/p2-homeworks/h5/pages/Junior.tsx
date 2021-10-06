6056bfb6092b1d835febcee8fdafab8451603a6e
BrilAlex <brilalex.it@yandex.ru>
2021-10-06 12:05:57 +0300

    Homework 07 done

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import HW7 from "../../h7/HW7";
 
 function Junior() {
     return (
         <div>
             <h1>Junior page</h1>
-            {/*<HW7/>*/}
+            <HW7/>
 
         </div>
     )
