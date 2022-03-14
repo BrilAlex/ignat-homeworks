c157a330112a2d712ef54ea7d9b5e4a01708d4b8
BrilAlex <brilalex.it@yandex.ru>
2022-03-14 11:54:42 +0300

    Homework 06 done

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -4,19 +4,19 @@ import {PATH} from "./Pages";
 import s from "./Header.module.css";
 
 function Header() {
-  const getNavLinkStyle = (NavData: {isActive: boolean}) => {
+  const getNavLinkStyle = (NavData: { isActive: boolean }) => {
     return NavData.isActive ? `${s.item} ${s.active}` : s.item;
   };
 
   return (
     <div className={s.navbar}>
-      <NavLink to={PATH.PRE_JUNIOR} className={(NavData)=> getNavLinkStyle(NavData)}>
+      <NavLink to={PATH.PRE_JUNIOR} className={(NavData) => getNavLinkStyle(NavData)}>
         Pre Junior
       </NavLink>
-      <NavLink to={PATH.JUNIOR} className={(NavData)=> getNavLinkStyle(NavData)}>
+      <NavLink to={PATH.JUNIOR} className={(NavData) => getNavLinkStyle(NavData)}>
         Junior
       </NavLink>
-      <NavLink to={PATH.JUNIOR_PLUS} className={(NavData)=> getNavLinkStyle(NavData)}>
+      <NavLink to={PATH.JUNIOR_PLUS} className={(NavData) => getNavLinkStyle(NavData)}>
         Junior Plus
       </NavLink>
       <div className={`${s.item} ${s.menuButton}`}>Menu</div>
